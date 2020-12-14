68018d9d41ad165cf9a0ba561cebfc52d65764c1
Daria Viter <viter.daria@gmail.com>
2020-12-14 21:21:58 +0200

    Update task-4.js

--- task-4.js
+++ task-4.js
@@ -5,16 +5,18 @@ let totalPrice = 1;
 let userInput = prompt('Сколько дроидов вы желаете приобрести?');
 
 
+
 if (userInput === null) {
   console.log('Отменено пользователем!');
 } else if (!isNaN(userInput)) {
-  totalPrice = userInput * pricePerDroid;
+  let droidNum = parseInt(userInput);
+  totalPrice = droidNum * pricePerDroid;
   if (credits < totalPrice) {
     console.log('Недостаточно средств на счету!');
   } else {
     credits -= totalPrice;
     console.log(
-      `Вы купили ${userInput} дроидов, на счету осталось ${credits} кредитов.`,
+      `Вы купили ${droidNum} дроидов, на счету осталось ${credits} кредитов.`,
     );
   }
 } else {
